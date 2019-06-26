f8a8601fd7f9e7bc12590e126b5e158c3121c281
JohnnyT <ubergeek3141@gmail.com>
2019-06-25 20:24:41 -0600

    Add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import App from './screens/App';
+//import App from './screens/App';
+import App from './App';
 import theme from './theme';
 import { ThemeProvider } from '@material-ui/styles';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
+    <Router>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </Router>
   </ThemeProvider>,
   document.querySelector('#root'),
 );
